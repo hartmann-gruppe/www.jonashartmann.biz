3b82c58c1c885d1808271f6a871f19e809e785ce
hartmann-gruppe <76873322+hartmann-gruppe@users.noreply.github.com>
2021-01-02 15:40:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const notionDocId = "f582e3b1298a4e4597c738255decb09b"
+const notionDocId = "33f34c811cca408782a29a1be168412b"
 
 fetch("https://potion-api.now.sh/html?id=" + notionDocId)
   .then(res => res.text())
