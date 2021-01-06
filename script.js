d053c61a05a3caf6461f0bcdeafaedbe2454fc7a
hartmann-group <talk@jonashartmann.biz>
2021-01-06 08:20:03 +0100

    4

--- script.js
+++
@@ -1,7 +0,0 @@
-const notionDocId = "33f34c811cca408782a29a1be168412b"
-
-fetch("https://potion-api.now.sh/html?id=" + notionDocId)
-  .then(res => res.text())
-  .then(text => {
-    document.querySelector("main").innerHTML = text
-  })
